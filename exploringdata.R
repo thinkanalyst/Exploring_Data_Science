eadd7d43184772ac5b2025c849a59a8dbb7b6f8a
Sandip Paul <sandeep.paul84@gmail.com>
2016-07-14 16:46:16 +0530

    checked system session

--- exploringdata.R
+++ exploringdata.R
@@ -4,7 +4,14 @@ summary(ex.1$Experience)
 install.packages("ggrepel")
 library(ggrepel)
 library(ggplot2)
+library(scales)
 ggplot(ex.1) +
     geom_histogram(aes(x=Country),
                    binwidth=5, fill="gray")
 
+ggplot(ex.1) +
+    geom_bar(aes(x=Experience), fill="gray") +
+    coord_flip() +
+    theme(axis.text.y=element_text(size=rel(0.8)))
+
+plot(Register.On ~ Last.Login, data=ex.1)
